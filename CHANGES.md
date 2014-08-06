d9561950b293857304237be7b515fc20ac41f1ba
James Arthur <thruflo@gmail.com>
2014-08-06 12:40:10 +0100

    release: 0.2.3.

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,8 @@
 
+## 0.2.2 / 0.2.3
+
+Bumped to ship package with and then without logging.
+
 ## 0.2.1
 
 Coerce latin-1 names to UTF-8.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'pyramid_geoip',
-    version = '0.2.2',
+    version = '0.2.3',
     description = 'Integrate MaxMind GeoIP data with a Pyramid web application.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
